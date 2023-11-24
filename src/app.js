f74719f7167b06ecb0a00340de47e701f724e2eb
Wendel <luccawendel25@gmail.com>
2023-11-24 11:51:56 -0300

    alteração de porta do servidor

--- src/app.js
+++ src/app.js
@@ -26,5 +26,5 @@ const swaggerSpec = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor rodando em http://localhost:4000/api-docs/#/`);
 });
